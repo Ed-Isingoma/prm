cd4a5f559305769c4d0144c6eee2d6f3b3b437a1
Ed <eddiisingoma@gmail.com>
2022-05-21 12:54:25 +0300

    Also this, prints using writeFileSync

--- main.js
+++ main.js
@@ -1,12 +1,66 @@
-const { app, BrowserWindow, ipcMain, Menu, MenuItem} = require('electron')
+const { app, BrowserWindow, ipcMain, Menu} = require('electron')
+const fs = require('fs')
 const path = require('path')
 
 const thePath = app.getPath('userData')
 ipcMain.on('bringLink', (ee, argu)=> {
     ee.returnValue = thePath
 })//ee is the event and we dont need to do anything with the argument
-ipcMain.on('quit', ()=> {app.quit()})
 
+//concerning the second browserwindow
+ipcMain.on('printThis', (e, thePage)=> {
+    fs.writeFileSync(__dirname + '/printable.html', thePage)
+    const win = new BrowserWindow({
+        width: 842,
+        height: 595,
+        webPreferences: {
+            preload: path.join(app.getAppPath(), 'preload.js'),
+        }
+    })
+    win.loadFile('printable.html')
+    win.webContents.on('did-finish-load', ()=> {
+        win.webContents.printToPDF({
+            printBackground: true,
+            landscape: true
+        }, (error, data) => {
+            if (error) throw error;
+            console.log('gonna writeFileSync')
+            //fs.writeFileSync(__dirname, data)
+        })
+    })
+})
+
+ipcMain.on('miniMenu', (e, id)=>{
+    const template = [{
+        label: 'Cut',
+        click() {
+            e.sender.send('miniMenu', 'cut ' + id)
+        }
+    }, {
+        label: 'Copy',
+        click(){
+            e.sender.send('miniMenu', 'copy ' + id)
+        }
+    }, {
+        label: 'Bold',
+        click(){
+            e.sender.send('miniMenu', 'bold ' + id)
+        }
+    }, {
+        label: 'Select All',
+        click() {
+            e.sender.send('miniMenu', 'selectAll ' + id)
+        }
+    }, /*{
+        label: 'Paste',
+        click() {
+            e.sender.send('miniMenu', 'paste ' + id)
+
+        }
+    }*/]
+    const smallMenu = Menu.buildFromTemplate(template)
+    smallMenu.popup(BrowserWindow.fromWebContents(e.sender))
+})
 ipcMain.on('showMenu', (e, msg)=> {
     const template = [{
         label: 'Edit entry',
@@ -24,21 +78,26 @@ ipcMain.on('showMenu', (e, msg)=> {
             e.sender.send('clicked', 'copy')
         }
     }, {
+        label: 'Select all',
+        click() {
+            e.sender.send('clicked', 'selectAll')
+        }
+    }, {
         label: 'Print selection',
         click() {
             e.sender.send('clicked', 'printSelection')
         }
-    }, {
+    }, /*{
         label: 'Settings',
         click() {
             e.sender.send('clicked', 'settings')
         }
-    }]
+    }*/]
     const theMenu = Menu.buildFromTemplate(template)
     theMenu.popup(BrowserWindow.fromWebContents(e.sender))
 })
 
-//initialising the app
+//initialising main browserWindow
 let win;
 function createWindow() {
     win = new BrowserWindow({
@@ -59,3 +118,4 @@ app.on('window-all-closed', ()=> {
 app.on('activate', ()=> {
     if (BrowserWindow.getAllWindows().length === 0) loadMainWindow();
 })
+ipcMain.on('quit', ()=> {app.quit()})
